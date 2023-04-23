b8e29d67341b1986a2fbaa2e01e56578109b17e2
AdityaGlobalLogic <aditya.goyal@globallogic.com>
2023-04-23 11:03:03 +0530

    Add project files.

---
+++ Book_Bazaar/Controllers/AuthenticationController.cs
@@ -0,0 +1,89 @@
+﻿using Book_Bazaar.Models;
+using Book_Bazaar.Models.Authentication.SignUp;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using User.Management.Service.Models;
+using User.Management.Service.Services;
+
+namespace Book_Bazaar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IEmailService _emailService;
+        private readonly IConfiguration _configuration;
+        public AuthenticationController( RoleManager<IdentityRole> roleManager, 
+            UserManager<IdentityUser> userManager, IEmailService emailService)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _emailService = emailService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] RegisterUser registerUser, string role)
+        {
+            var userExist = await _userManager.FindByEmailAsync(registerUser.Email);
+            if(userExist != null)
+            {
+                return StatusCode(StatusCodes.Status403Forbidden, 
+                    new Response { Status = "Error", Message = "user already exists!" });
+            }
+
+            IdentityUser user = new()
+            {
+                Email = registerUser.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = registerUser.FirstName + registerUser.LastName
+            };
+
+            if(await _roleManager.RoleExistsAsync(role))
+            {
+                var result = await _userManager.CreateAsync(user, registerUser.Password);
+                if (!result.Succeeded)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError,
+                        new Response { Status = "Error", Message = "user failed to create!" });
+                }
+
+                await _userManager.AddToRoleAsync(user, role);
+
+                var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                var confirmationLink = Url.Action(nameof(ConfirmEmail), "Authentication", new { token, email = user.Email }, Request.Scheme);
+                var message = new Message(new string[] { user.Email }, "Confirmation email link", confirmationLink!);
+                _emailService.SendEmail(message);
+
+
+                return StatusCode(StatusCodes.Status200OK,
+                        new Response { Status = "Success", Message = $"User created and Email sent to {user.Email} successfully" });
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                        new Response { Status = "Error", Message = "This role does not exist!" });
+            }           
+        }
+
+        [HttpGet("ConfirmEmail")]
+        public async Task<IActionResult> ConfirmEmail(string token, string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if(user != null)
+            {
+                var result = await _userManager.ConfirmEmailAsync(user, token);
+                if(result.Succeeded)
+                {
+                    return StatusCode(StatusCodes.Status200OK,
+                        new Response { Status = "Success", Message = "Email verified successfully" });
+                }
+            }
+
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                        new Response { Status = "Error", Message = "This user does not exist!" });
+        }
+    }
+}
---
+++ Book_Bazaar/Models/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace Book_Bazaar.Models
+{
+    public class Response
+    {
+        public string? Status { get; set; }
+        public string? Message { get; set; }
+    }
+}
