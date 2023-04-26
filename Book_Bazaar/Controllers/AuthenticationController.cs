c62607d7e1ce9f19b58583b8fd91ca3bb2fe0ee3
Vishvajeet Singh <vishvajeet.singh2@globallogic.com>
2023-04-26 11:13:49 +0530

    new features by vishvajeet

--- Book_Bazaar/Controllers/AuthenticationController.cs
+++ Book_Bazaar/Controllers/AuthenticationController.cs
@@ -1,10 +1,12 @@
 ﻿using Book_Bazaar.Models;
 using Book_Bazaar.Models.Authentication.Login;
 using Book_Bazaar.Models.Authentication.SignUp;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
+using System.ComponentModel.DataAnnotations;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
@@ -18,15 +20,17 @@ namespace Book_Bazaar.Controllers
     public class AuthenticationController : ControllerBase
     {
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly IEmailService _emailService;
         private readonly IConfiguration _configuration;
         public AuthenticationController( RoleManager<IdentityRole> roleManager, 
-            UserManager<IdentityUser> userManager, IEmailService emailService, IConfiguration configuration)
+            UserManager<IdentityUser> userManager, IEmailService emailService, IConfiguration configuration, SignInManager<IdentityUser> signInManager)
         {
             _roleManager = roleManager;
             _userManager = userManager;
             _emailService = emailService;
+            _signInManager= signInManager;
             _configuration = configuration;
         }
 
@@ -44,7 +48,8 @@ namespace Book_Bazaar.Controllers
             {
                 Email = registerUser.Email,
                 SecurityStamp = Guid.NewGuid().ToString(),
-                UserName = registerUser.FirstName + registerUser.LastName
+                UserName = registerUser.FirstName + registerUser.LastName,
+                TwoFactorEnabled=true
             };
 
             if(await _roleManager.RoleExistsAsync(role))
@@ -97,6 +102,17 @@ namespace Book_Bazaar.Controllers
         public async Task<IActionResult> Login([FromBody] LoginModel loginModel)
         {
             var user = await _userManager.FindByEmailAsync(loginModel.Email);
+            if (user.TwoFactorEnabled)
+            {
+                await _signInManager.SignOutAsync();
+                await _signInManager.PasswordSignInAsync(user, loginModel.Password, false, true);
+                var token = await _userManager.GenerateTwoFactorTokenAsync(user, "Email");
+                var message = new Message(new string[] { user.Email }, "OTP Confirmation", token);
+                _emailService.SendEmail(message);
+                return StatusCode(StatusCodes.Status200OK,
+                    new Response { Status = "Success", Message = $"An OTP is sent to your Email : {user.Email}" });
+
+            }
             if (user != null && await _userManager.CheckPasswordAsync(user, loginModel.Password))
             {
                 var authClaims = new List<Claim>
@@ -109,6 +125,7 @@ namespace Book_Bazaar.Controllers
                 {
                     authClaims.Add(new Claim(ClaimTypes.Role, role));
                 }
+               
                 var jwtToken = GetToken(authClaims);
                 return Ok(
                     new
@@ -118,6 +135,93 @@ namespace Book_Bazaar.Controllers
                     });
             }
             return Unauthorized();
+        }
+
+        [HttpPost]
+        [Route("login-2FA")]
+        public async Task<IActionResult> LoginWithOTP(string code,string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            var signIn = await _signInManager.TwoFactorSignInAsync("Email", code, false, false);
+            if (signIn.Succeeded)
+            {
+                if (user != null)
+                {
+                    var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,user.Email),
+                    new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+                };
+                    var userRoles = await _userManager.GetRolesAsync(user);
+                    foreach (var role in userRoles)
+                    {
+                        authClaims.Add(new Claim(ClaimTypes.Role, role));
+                    }
+
+                    var jwtToken = GetToken(authClaims);
+                    return Ok(
+                        new
+                        {
+                            token = new JwtSecurityTokenHandler().WriteToken(jwtToken),
+                            expiration = jwtToken.ValidTo
+                        });
+                }
+
+            }
+            return StatusCode(StatusCodes.Status404NotFound,
+                    new Response { Status = "Error", Message = $"Invalid Code." });
+
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("forgot-password")]
+        public async Task<IActionResult> ForgotPassword([Required] string email)
+        {
+            var user= await _userManager.FindByEmailAsync(email);
+            if (user != null)
+            {
+                var token= await _userManager.GeneratePasswordResetTokenAsync(user);
+                var forgotPasswordlink = Url.Action(nameof(ResetPassword), "Authentication", new { token, email = user.Email }, Request.Scheme);
+                var message = new Message(new string[] { user.Email }, "Forgot Password Link : ", forgotPasswordlink);
+                _emailService.SendEmail(message);
+                return StatusCode(StatusCodes.Status200OK,
+                        new Response { Status = "Success", Message = $"Password change request is sent on mail : {user.Email}.Kindly open your mail and click on the link" });
+            }
+            return StatusCode(StatusCodes.Status400BadRequest,
+                        new Response { Status = "Error", Message = $"Couldn't send link to mail, kindly try again." });
+        }
+
+        [HttpGet("reset-password")]
+        public async Task<IActionResult> ResetPassword(string token, string email)
+        {
+            var model= new ResetPassword { Token=token, Email=email };
+
+            return Ok(new { model });
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("reset-password")]
+        public async Task<IActionResult> ResetPassword(ResetPassword resetPassword)
+        {
+            var user = await _userManager.FindByEmailAsync(resetPassword.Email);
+            if (user != null)
+            {
+                var resetPassResult = await _userManager.ResetPasswordAsync(user, resetPassword.Token, resetPassword.Email);
+                if (!resetPassResult.Succeeded)
+                {
+
+                    foreach(var error in resetPassResult.Errors)
+                    {
+                        ModelState.AddModelError(error.Code, error.Description);
+                    }
+                    return Ok(ModelState);
+                }
+
+                return StatusCode(StatusCodes.Status200OK,
+                        new Response { Status = "Success", Message = $"Password changed succesfully" });
+            }
+            return StatusCode(StatusCodes.Status400BadRequest,
+                        new Response { Status = "Error", Message = $"Couldn't send link to mail, kindly try again." });
         }
 
         private JwtSecurityToken GetToken(List<Claim> authClaims)
--- Book_Bazaar/Migrations/20230424145345_init.Designer.cs
+++ Book_Bazaar/Migrations/20230424145345_init.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Book_Bazaar.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20230422164839_RolesSeeded")]
-    partial class RolesSeeded
+    [Migration("20230424145345_init")]
+    partial class init
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -54,21 +54,21 @@ namespace Book_Bazaar.Migrations
                     b.HasData(
                         new
                         {
-                            Id = "4cbab96e-9095-4ecc-ae7f-54fceff206e3",
+                            Id = "9e96d54b-e21a-4733-8359-aaf517e5deb1",
                             ConcurrencyStamp = "1",
                             Name = "Admin",
                             NormalizedName = "Admin"
                         },
                         new
                         {
-                            Id = "a57d9d55-b2b3-4325-947a-b3551baf3cd9",
+                            Id = "a9803032-5425-44fb-a590-617b2f5c7b0a",
                             ConcurrencyStamp = "2",
                             Name = "User",
                             NormalizedName = "User"
                         },
                         new
                         {
-                            Id = "8075fd8b-5859-4cff-ab60-0345997ca369",
+                            Id = "af6f4f49-bb6d-447b-882b-8b0a0807686e",
                             ConcurrencyStamp = "3",
                             Name = "Vendor",
                             NormalizedName = "Vendor"
